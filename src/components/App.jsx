9001c8bdec82251e3bd5dacacd066774c9a0c3fc
Melnyk Oleksii <112346850+Oleksii-Melnyk-1993@users.noreply.github.com>
2023-07-21 22:46:04 +0300

    add component to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import { nanoid } from 'nanoid';
 
 import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 
 export class App extends Component {
