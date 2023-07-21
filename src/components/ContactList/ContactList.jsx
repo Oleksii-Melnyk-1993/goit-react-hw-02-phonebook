0d121c11356c4aac7546a244b7ae491ea02539cb
Melnyk Oleksii <112346850+Oleksii-Melnyk-1993@users.noreply.github.com>
2023-07-21 22:39:16 +0300

    why?

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,5 +1,22 @@
-import Proptypes from 'prop-types';
+import PropTypes from 'prop-types';
+import { ContactListItem } from './ContactItemList/ContactListItem';
 
 export const ContactList = ({ filteredContactList, removeContact }) => {
-  return <ul></ul>;
+  return (
+    <ul>
+      {filteredContactList.map(contact => (
+        <ContactListItem
+          key={contact.id}
+          contact={contact}
+          removeContact={removeContact}
+        />
+      ))}
+    </ul>
+  );
+};
+
+ContactList.propTypes = {
+  filteredContactList: PropTypes.arrayOf(PropTypes.object.isRequired)
+    .isRequired,
+  removeContact: PropTypes.func.isRequired,
 };
