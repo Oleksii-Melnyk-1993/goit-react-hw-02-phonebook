75c0464420414a997d8106e92418f07c11ef21fa
Melnyk Oleksii <112346850+Oleksii-Melnyk-1993@users.noreply.github.com>
2023-07-21 22:44:00 +0300

    maybe finish

--- src/components/ContactList/ContactItemList/ContactListItem.jsx
+++ src/components/ContactList/ContactItemList/ContactListItem.jsx
@@ -1,5 +1,21 @@
 import PropTypes from 'prop-types';
 
-export const ContactListItem = ({ }) => {
-    return ()
-}
+export const ContactListItem = ({ contact, removeContact }) => {
+  return (
+    <li key={contact.id}>
+      {contact.name} : {contact.number}
+      <button type="button" onClick={() => removeContact(contact.id)}>
+        Delete
+      </button>
+    </li>
+  );
+};
+
+ContactListItem.propTypes = {
+  contact: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    number: PropTypes.string.isRequired,
+  }).isRequired,
+  removeContact: PropTypes.func.isRequired,
+};
